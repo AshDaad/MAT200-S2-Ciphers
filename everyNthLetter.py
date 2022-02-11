eee4e576281cdb30df48c20d78ccc72d01b53fc2
Alex <alex.fritzacus@gmail.com>
2022-02-11 13:13:48 -0600

    Add files via upload

---
+++ everyNthLetter.py
@@ -0,0 +1,14 @@
+#'HowaboutreleasingTonythegangchiefHedon’tlieandisnotthetrueslayereitherLetusbefriendlyHisvoiceisneeded'[::5]
+
+def main():
+    while True:
+        keyA = random.randint(2, len(SYMBOLS))
+        keyB = random.randint(2, len(SYMBOLS))
+        if cryptomath.gcd(keyA, len(SYMBOLS)) == 1:
+            return keyA * len(SYMBOLS) + keyB
+
+
+# If affineCipher.py is run (instead of imported as a module) call
+# the main() function.
+if __name__ == '__main__':
+    main()
